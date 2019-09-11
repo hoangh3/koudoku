fe2c1ef23212308a24a88ed429088114c3f1c0cf
SBOX <hoangtnth@gmail.com>
2019-09-11 13:59:10 +0700

    add overrides

--- lib/koudoku/engine.rb
+++ lib/koudoku/engine.rb
@@ -10,6 +10,12 @@ module Koudoku
       g.helper false
     end
 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/overrides/**/*_override*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
+
     initializer 'koudoku.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         include Koudoku::ApplicationHelper
